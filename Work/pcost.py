673e9c43ed4d4147a709b3e23da07a06cb04c8e1
Bottom-Pump <2042239448@qq.com>
2025-08-01 14:52:34 +0800

    Exercise 1.30: Turning a script into a function

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,13 +1,17 @@
 # pcost.py
 #
 # Exercise 1.27
-sum = 0
-f = open("Data/portfolio.csv", 'rt')
-headers = next(f)
-for line in f:
-    row = line.split(',')
-    shares = int(row[1])
-    price = float(row[2].strip())
-    sum += shares * price
-print(f'Total cost {sum}')
-f.close()
+def portfolio_cost(filename):
+    sum = 0
+    f = open(filename, 'rt')
+    headers = next(f)
+    for line in f:
+        row = line.split(',')
+        shares = int(row[1])
+        price = float(row[2].strip())
+        sum += shares * price
+    # print(f'Total cost {sum}')
+    f.close()
+    return sum
+cost = portfolio_cost(('Data/portfolio.csv'))
+print('Total cost:', cost)
