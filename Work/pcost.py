5509f05b220200ddceb7d9cf454025a15cb3b360
Bottom-Pump <2042239448@qq.com>
2025-08-01 15:30:12 +0800

    change the hard-coded filename

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,6 +2,7 @@
 #
 # Exercise 1.27
 import csv
+import sys
 def portfolio_cost(filename):
     sum = 0
     f = open(filename, 'rt')
@@ -13,9 +14,13 @@ def portfolio_cost(filename):
             price = float(row[2].strip())
             sum += shares * price
         except ValueError as e:
-            print(f'Warning: Skip invalid lines [{line.strip()}]:{e}')
+            print(f'Warning: Skip invalid lines {row}:{e}')
     # print(f'Total cost {sum}')
     f.close()
     return sum
-cost = portfolio_cost(('Data/portfolio.csv'))
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+cost = portfolio_cost((filename))
 print('Total cost:', cost)
