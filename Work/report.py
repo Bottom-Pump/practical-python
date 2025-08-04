975d3e1f8200cf2b17a0cb6620932d78ab48726b
Bottom-Pump <2042239448@qq.com>
2025-08-04 10:01:34 +0800

    Exercise 2.4,2.5 ^ modified report.py

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,38 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+
+portfolio = []
+prices = {}
+
+def read_portfolio(filename):
+    'Open a given portfolio file and read it into a list of tuples'
+    with open(filename,'rt') as f:
+        rows = csv.reader(f)
+        headers  = next(rows)
+        for row in rows:
+            holding = {}
+            holding['name'] = row[0]
+            holding['shares'] = int(row[1])
+            holding['price'] = float(row[2])
+            portfolio.append(holding)
+    return portfolio
+
+def read_prices(filename):
+    ''' 
+    read a set of prices into a dictionary 
+    keys: stock names
+    values: prices.
+    '''
+    with open(filename,'rt') as f:
+        rows = csv.reader(f)
+        header = next(rows)
+        for row in rows:
+            try:
+                prices[row[0]] = float(row[1])
+            except Exception as e:
+                print(e)
+    return prices
+                
+    
