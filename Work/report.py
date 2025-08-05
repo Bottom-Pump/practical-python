95c04695e1ad85d7dcce92f36344bb18da920883
Bottom-Pump <2042239448@qq.com>
2025-08-05 18:05:31 +0800

    fixed bugs in `report.py` caused by global variables and increased top function `portfolio_report(portfolio_filename, prices_filename)`

--- Work/report.py
+++ Work/report.py
@@ -3,12 +3,9 @@
 # Exercise 2.4
 import csv
 
-portfolio = []
-prices = {}
-report = []
-
 def read_portfolio(filename):
-    'Open a given portfolio file and read it into a list of tuples'
+    'Open a given portfolio file and read it into a list of tuples (name,shares,price)'
+    portfolio = []
     with open(filename,'rt') as f:
         rows = csv.reader(f)
         headers  = next(rows)
@@ -31,24 +28,29 @@ def read_prices(filename):
     keys: stock names
     values: prices.
     '''
+    prices = {}
     with open(filename,'rt') as f:
         rows = csv.reader(f)
-        for row in rows:
+        for rowno,row in enumerate(rows,start=2):
             try:
                 prices[row[0]] = float(row[1])
             except Exception as e:
-                print(e)
+                print(f'Something get wrong in reading {filename}: row:{rowno}')
     return prices
                 
 def make_report(portfolio,prices):
     '[portfolio]:list of the stock price you purchase \n [prices]: tuples of the current share price of the stock'
+    report = []
     for stock in portfolio:
         name = stock['name']
         shares = stock['shares']
         price = prices[name]
         change = price - stock['price']
         report.append((name,shares,price,change))
-    # print a formatted table
+    return report
+
+def print_report(report):
+    'print a formatted table'
     headers = ('Name', 'Shares', 'Price', 'Change')
     for i in headers:
         print('%10s' % i,end=' ')
@@ -60,6 +62,9 @@ def make_report(portfolio,prices):
     for name, shares, price, change in report:
         price_str = f"${price:.2f}"
         print(f'{name:>10s} {shares:>10d} {price_str:>10s} {change:>10.2f}')
-    return report
 
-    
+def portfolio_report(portfolio_filename, prices_filename):
+    portfolio = read_portfolio(portfolio_filename)
+    prices = read_prices(prices_filename)
+    report = make_report(portfolio,prices)
+    print_report(report)
