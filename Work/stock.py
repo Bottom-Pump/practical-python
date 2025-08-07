3d44055a388c6b0bcf7908bdc478d675d0f48473
Bottom-Pump <2042239448@qq.com>
2025-08-07 17:27:29 +0800

    Exercise 5.7: Properties and Setters

--- Work/stock.py
+++ Work/stock.py
@@ -8,6 +8,16 @@ class Stock:
     def __repr__(self):
         return f'Stock({self.name!r}, {self.shares!r}, {self.price!r})'
     @property
+    def shares(self):
+        return self._shares
+    
+    @shares.setter
+    def shares(self,value):
+        if not isinstance(value, int):
+            raise TypeError('Expected int')
+        self._shares = value    
+    
+    @property
     def cost(self):
         return self.shares * self.price
     def sell(self, nshares):
